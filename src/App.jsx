83d90fedbe36353a7e567ddafce18e22a6a26e95
B-VIGNESH18 <vigneshbalu382002@gmail.com>
2025-06-11 14:09:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'; // ✅
+import './App.css'; 
 import { Route, Routes } from 'react-router-dom';
 import IndexPage from './Pages/IndexPage';
 import LoginPage from './Pages/LoginPage';
